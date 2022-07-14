b8700ea047be5c3d777e63e19b8c605ec341fc9e
Leonardo Terrero Perez <terreroperezleonardo@gmail.com>
2022-07-14 08:59:30 +0800

    Add project files.

---
+++ Sinvex.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sinvex.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
